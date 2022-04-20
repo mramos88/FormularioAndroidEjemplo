7e725fd941505560bd49b8a74c809671b95156f5
Your Name <you@example.com>
2022-04-19 21:22:41 -0300

    Clase 1 y 2 ejemplo formulario basico en android

---
+++ Clase01/app/src/main/java/ar/com/sistema/clase01/MainActivity.java
@@ -0,0 +1,26 @@
+package ar.com.sistema.clase01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = super.findViewById(R.id.btnGuardar);
+
+        btn.setOnClickListener(new MyClickListener(this));
+
+
+
+
+
+
+    }
+}
---
+++ Clase01/app/src/main/java/ar/com/sistema/clase01/Persona.java
@@ -0,0 +1,50 @@
+package ar.com.sistema.clase01;
+
+class Persona {
+   private String nombre;
+   private String apellido;
+   private Integer dni;
+   private String sexo;
+
+   public String getNombre() {
+      return nombre;
+   }
+
+   public void setNombre(String nombre) {
+      this.nombre = nombre;
+   }
+
+   public String getApellido() {
+      return apellido;
+   }
+
+   public void setApellido(String apellido) {
+      this.apellido = apellido;
+   }
+
+   public Integer getDni() {
+      return dni;
+   }
+
+   public void setDni(Integer dni) {
+      this.dni = dni;
+   }
+
+   public String getSexo() {
+      return sexo;
+   }
+
+   public void setSexo(String sexo) {
+      this.sexo = sexo;
+   }
+
+   @Override
+   public String toString() {
+      return "Persona{" +
+              "nombre='" + nombre + '\'' +
+              ", apellido='" + apellido + '\'' +
+              ", dni=" + dni +
+              ", sexo='" + sexo + '\'' +
+              '}';
+   }
+}
